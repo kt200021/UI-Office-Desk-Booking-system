d8b253d1709cf49895b0a5d66b5ac605393492ff
Karsh Tandon <tandonkarsh@gmail.com>
2022-07-25 19:49:04 +0530

    js added

---
+++ js/selectSeat.js
@@ -0,0 +1,12 @@
+import { bangaloreData } from "./data";
+import {
+  locationValue,
+  dateValue,
+  monthValue,
+  deskNumber,
+  deskRow,
+} from "./index";
+console.log(deskRow);
+console.log(deskNumber);
+console.log(monthValue);
+console.log(dateValue);
